ca56ff5e5723a59bd6ec3f95f57f5857bcc7eb25
NSharp-mahajan <mahajannatansh@gmail.com>
2026-05-22 17:29:19 +0530

    Measurment ID added

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,20 +1,29 @@
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize(import.meta.env.VITE_GA_ID);
+  const gaId = import.meta.env.VITE_GA_ID;
+  if (gaId) {
+    ReactGA.initialize(gaId);
+  } else {
+    console.warn("GA ID not found in environment variables.");
+  }
 };
 
 export const pageView = () => {
-  ReactGA.send({
-    hitType: "pageview",
-    page: window.location.pathname,
-  });
+  if (import.meta.env.VITE_GA_ID) {
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname,
+    });
+  }
 };
 
 export const trackEvent = (category, action, label = "") => {
-  ReactGA.event({
-    category,
-    action,
-    label,
-  });
+  if (import.meta.env.VITE_GA_ID) {
+    ReactGA.event({
+      category,
+      action,
+      label,
+    });
+  }
 };
